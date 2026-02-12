de7d6ba3a8580a8c3ce7d0b995adc876ec4f44f0
Svetlana Makarova <CBETA-MAKAPOBA@yandex.ru>
2026-02-12 19:26:58 +0300

    add internal function iter for stylish

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -28,70 +28,42 @@ function stringify(mixed $item, int $depth = 1): string
     }
 
     if (gettype($item) === 'object') {
-        $depth += 1;
         $properties = get_object_vars($item);
 
         $result = array_map(function ($key, $property) use ($depth) {
-            return makeIndent($depth) . $key . ': ' . stringify($property, $depth);
+            return makeIndent($depth + 1) . $key . ': ' . stringify($property, $depth + 1);
         }, array_keys($properties), array_values($properties));
 
-        return "{\n" . implode("\n", $result) . "\n" . makeIndent($depth - 1) . "}";
+        return "{\n" . implode("\n", $result) . "\n" . makeIndent($depth) . "}";
     }
 
     return $item;
 }
 
-function render(array $comparisons, int $depth = 1): string
+function iter(array $comparisons, int $depth = 1): string
 {
-    //$result = [];
-    $indent = makeIndent($depth, 2);
-
-    /*$result = array_map(
-        function ($node) {
-            $key = stringify($node['key']);
+    $result = array_map(
+        function ($node) use ($depth) {
+            $key = $node['key'];
+            $indent = makeIndent($depth, 2);
 
             return match ($node['status']) {
-                'nested' => "{$indent}  {$node['key']}: " . render($node['children'], $depth + 1),
+                'nested' => makeIndent($depth) . "{$node['key']}: " . iter($node['children'], $depth + 1),
                 'added' => "{$indent}+ {$key}: " . stringify($node['newValue'], $depth),
                 'deleted' => "{$indent}- {$key}: " . stringify($node['oldValue'], $depth),
-                //в changed нужно добавить 2 значения: oldValue и newValue
-                'changed' => "{$indent}- {$key}: " . stringify($node['oldValue'], $depth) . "\n{$indent}+ {$key}: ". stringify($node['newValue'], $depth),
-                //"{$indent}+ {$key}: " . stringify($node['newValue'], $depth)
+                'changed' => "{$indent}- {$key}: " . stringify($node['oldValue'], $depth) .
+                    "\n{$indent}+ {$key}: " . stringify($node['newValue'], $depth),
+                'unchanged' => "{$indent}  {$key}: " . stringify($node['value'], $depth),
             };
         },
         $comparisons
-    );*/
-    foreach ($comparisons as $comparison) {
-        $key = stringify($comparison['key']);
-
-        if ($comparison['status'] === 'nested') {
-            $result[] = "{$indent}  {$key}: " . render($comparison['children'], $depth + 1);
-        } else {
-            if ($comparison['status'] === 'changed') {
-                $result[] = "{$indent}- {$key}: " .
-                    stringify($comparison['oldValue'], $depth);
-
-                $result[] = "{$indent}+ {$key}: " .
-                    stringify($comparison['newValue'], $depth);
-            }
-
-            if ($comparison['status'] === 'unchanged') {
-                $result[] = "{$indent}  {$key}: " .
-                    stringify($comparison['value'], $depth);
-            }
-
-            if ($comparison['status'] === 'deleted') {
-                $result[] = "{$indent}- {$key}: " .
-                    stringify($comparison['oldValue'], $depth);
-            }
-
-            if ($comparison['status'] === 'added') {
-                $result[] = "{$indent}+ {$key}: " .
-                    stringify($comparison['newValue'], $depth);
-            }
-        }
-    }
+    );
 
     $indentForClosedBrace = makeIndent($depth - 1);
     return "{\n" . implode("\n", $result) . "\n{$indentForClosedBrace}}";
+}
+
+function render($tree): string
+{
+    return iter($tree, 1);
 }
