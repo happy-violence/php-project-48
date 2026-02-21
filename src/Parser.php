bb2d021e231f9f2cdef8a3c514e2da62ba2f1169
Svetlana Makarova <CBETA-MAKAPOBA@yandex.ru>
2026-02-21 15:09:40 +0300

    edit the error message

--- src/Parser.php
+++ src/Parser.php
@@ -10,7 +10,7 @@ function parse(string $data, string $format): object
         'json' => json_decode($data),
         'yaml', 'yml' => (object) Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP),
         default => throw new \Exception(
-            "Extension {$format} is not supported. Choose 'json', 'yaml' or 'yml' extension"
+            "Extension is not supported. Choose 'json', 'yaml' or 'yml' extension"
         ),
     };
 }
