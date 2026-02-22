8c987620e3c02922704239cb96cd5e3bde7cdc7f
Svetlana Makarova <CBETA-MAKAPOBA@yandex.ru>
2026-02-22 12:04:21 +0300

    add exception

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,7 +2,8 @@
 
 namespace Differ\Formatters\Json;
 
-function render(array $comparisons): false|string
+function render(array $comparisons): string
 {
-    return json_encode($comparisons);
+    return json_encode($comparisons) ? json_encode($comparisons) :
+        throw new \Exception("Error encoding data to JSON: " . json_last_error_msg());
 }
